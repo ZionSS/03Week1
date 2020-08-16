0e0b1f8ddd5a40f7995eb84c1a28daa7eff792a8
Phacharaphol Chokkhun <66435789+ZionSS@users.noreply.github.com>
2020-08-16 23:51:28 +0700

    Update Source.cpp

--- 03Week1/Source.cpp
+++ 03Week1/Source.cpp
@@ -7,5 +7,11 @@ int main() {
     scanf("%lf", &h);
     printf("Input Base lenght:");
     scanf("%lf", &b);
+    if(h>0 && b>0){
     printf("Area of triangle is %.2lf", 0.5 * h * b);
+    }
+     else if(h <= 0 || b  <= 0)
+     {
+        printf("Error");
+     }  
 }
